071d09522dd9a6170b902a00a83a7ae3c9f4811a
andressagomes26 <andressa_gomes.12@hotmail.com>
2020-08-11 09:19:40 -0300

    initial commit

--- Fase03/Modulo02/Desafio01/public/scripts.js
+++
@@ -1,9 +0,0 @@
-// Responsável por ativar o menu
-const pageAtual = location.pathname
-const menuItems = document.querySelectorAll('header .links a')
-
-for(item of menuItems){
-    if(pageAtual.includes(item.getAttribute("href"))){
-        item.classList.add('active')
-    }
-}
