27b6264f0d7c6557b5db5e4a15590ed35a0024a5
Andressa Gomes <andressa_gomes.12@hotmail.com>
2021-10-01 17:43:50 -0300

    Finalizando desafio 3-3: Página de descrição do curso

--- Desafios/Fase02/modulo03/public/scripts.js
+++ Desafios/Fase02/modulo03/public/scripts.js
@@ -1,30 +1,8 @@
 const cards = document.querySelectorAll(".card")
-const modalOverlay = document.querySelector(".modal-overlay")
-const modal = document.querySelector(".modal")
-
-const iframe = document.querySelector('iframe')
-const close = document.querySelector('#close')
-const maximize = document.querySelector('#maximize')
-
 
 for(let card of cards){
     card.addEventListener('click', function(){
         const idIframe = card.getAttribute("id")
-        modalOverlay.classList.add('active')
-        iframe.src = `https://blog.rocketseat.com.br/${idIframe}`
-        
-        modal.classList.remove('maximize-active') 
+        window.location.href = `/course/${idIframe}`
     })
 }
-
-close.addEventListener('click', function(){
-    modalOverlay.classList.remove('active')
-})
-
-maximize.addEventListener('click', function(){
-    if(modal.classList.contains('maximize-active')){
-        modal.classList.remove('maximize-active')
-    }else(
-        modal.classList.add('maximize-active')
-    )
-})
--- Desafios/Fase02/modulo03/server.js
+++ Desafios/Fase02/modulo03/server.js
@@ -7,19 +7,32 @@ const cursos = require('./data')
 const server = express()
 
 server.use(express.static('public'))
-server.set('view engine', "njk")
 
+server.set('view engine', "njk")
 nunjucks.configure('views', {
-    express: server
+    express: server,
+    autoescape: false,
+    noCache: true
 })
 
-server.get('/', function(req, res){
+server.get("/", function(req, res){
+    return res.render("courses", {ItensCursos: cursos})
+})
+
+server.get("/course/:id", function(req, res) {
+    const id = req.params.id;
+
+    const curso = cursos.find((curso) => curso.id == id) 
+
+    if(!curso) return res.status(404).render("not-found");
+    
+    return res.render("course", {item: cursos})
+});
+
+server.get('/about', function(req, res){
     return res.render("about")
 })
 
-server.get("/courses", function(req, res){
-    return res.render("courses", {ItensCursos: cursos})
-})
 
 server.use(function(req, res) {
     res.status(404).render("not-found");
