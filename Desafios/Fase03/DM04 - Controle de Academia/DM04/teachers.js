0c2388d515ab68ff7cc89e28addec41e08040f57
Andressa Gomes <andressa_gomes.12@hotmail.com>
2021-10-23 20:24:37 -0300

    Desafio 4-3 incompleto

---
+++ Desafios/Fase03/DM04 - Controle de Academia/DM04/teachers.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const data = require('./data.json')
+
+exports.post =  function(req, res){
+    const keys = Object.keys(req.body) 
+    
+    for(key of keys){
+        if(req.body[key] == ""){  
+            return res.send('Please, fill in all fields!')
+        }
+    }
+
+    let { avatar_url, name, birth, education, typeClass, area } = req.body
+    
+    birth = Date.parse(birth)
+    const id = Number(data.teachers.length + 1)
+
+    data.teachers.push({
+        id,
+        avatar_url, 
+        name, 
+        birth, 
+        education, 
+        typeClass, 
+        area
+    })
+    
+    fs.writeFile('data.json', JSON.stringify(data, null, 4), function(err){
+        if (err) return res.send("Write file error!")
+
+        return res.redirect("/teachers")
+     })
+}
